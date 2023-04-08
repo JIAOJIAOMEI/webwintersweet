252eeba3c6d57b20f9a54edcefb7739a3df0fe1b
JIAOJIAOMEI <90816748+JIAOJIAOMEI@users.noreply.github.com>
2023-04-08 06:54:47 +0200

    update

--- themes/next/source/js/outdate.js
+++ themes/next/source/js/outdate.js
@@ -17,11 +17,11 @@
         //note warning 以及 note danger 是 Next 主题的自定义模板语法，如果使用其他主题，请自行更改样式以达到最佳显示效果
     if (interval > warningDay * 3600 * 24 * 1000 && interval < errorDay * 3600 * 24 * 1000) {
         posts[0].innerHTML = '<div class="note warning">' +
-            '<h5>文章时效性提示</h5><p>这是一篇发布于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。</p>' +
+          '<h5>Reminder: Article Age</h5><p>This article was published ' + days + ' days ago, and some information may have changed. Please keep this in mind when reading.</p>' +
             '</div>' + posts[0].innerHTML;
     } else if (interval >= errorDay * 3600 * 24 * 1000) {
         posts[0].innerHTML = '<div class="note danger">' +
-            '<h5>文章时效性提示</h5><p>这是一篇发布于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。</p>' +
+          '<h5>Reminder: Article may be outdated</h5><p>This article was published ' + days + ' days ago, and some information may have changed. Please use discretion when referencing it.</p>' +
             '</div>' + posts[0].innerHTML;
     }
 })();
